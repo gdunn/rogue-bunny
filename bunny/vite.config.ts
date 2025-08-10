eee5db33c692a90919f59ae47f096427146cc777
Geoffrey Dunn <geoff@warmage.com>
2025-08-10 08:31:09 +0000

    Fix style error

--- bunny/vite.config.ts
+++ bunny/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rogue-bunny/',
+  base: "/rogue-bunny/",
   server: {
     port: 8080,
     open: true,
